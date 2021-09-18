fd1ab9f609136ec082c93b8639ebafe406d2de86
Muhammad Khoirul Huda <khairulhuda@ruangguru.com>
2021-09-18 15:32:50 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mkhuda/halo
+
+go 1.14
+
+require (
+	github.com/google/btree v1.0.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/peterbourgon/diskv"
+)
+
+func main() {
+	var key, value string
+
+	fmt.Println("::Using Diskv")
+
+	currentDisk := initDiskv()
+
+	fmt.Println("::::key")
+	fmt.Print("Enter key: ")
+
+	// Input key name
+	k, err := fmt.Scanln(&key)
+	if err != nil {
+		justError(k, err)
+		return
+	}
+
+	// currentDisk.Write(key, []byte{`a`})
+	fmt.Println("::::value")
+	fmt.Print("Enter the value: ")
+
+	// Input value
+	v, err := fmt.Scanln(&value)
+	if err != nil {
+		justError(v, err)
+		return
+	}
+	writeBuf, sync := bytes.NewBufferString(value), true
+	if err := currentDisk.WriteStream(key, writeBuf, sync); err != nil {
+		fmt.Printf("Write error: %v", err)
+	}
+}
+
+func initDiskv() *diskv.Diskv {
+	flatTransform := func(s string) []string { return []string{} }
+
+	disk := diskv.New(diskv.Options{
+		BasePath:     "localdisk",
+		Transform:    flatTransform,
+		CacheSizeMax: 1024 * 1024,
+	})
+
+	return disk
+}
+
+func justError(id int, err error) {
+	fmt.Printf("Error: %v %v %v", id, os.Stderr, err)
+}
